2d26ce1174cb82f0f612e41915af3cc6173ab9c4
DanielChimalGarcia <danny.chiga@gmail.com>
2023-05-19 19:13:50 -0600

    update

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import MenuLateral from '../components/MenuLateral'
-
+import React from "react";
+import MenuLateral from "../components/MenuLateral";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
     <div>
-      <MenuLateral/>
-        <h1>Bienvenid@s</h1>
+      <MenuLateral />
+      <Container
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <h1>Daniel Chimal García</h1>
+        <h3>Grupo 3801</h3>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
